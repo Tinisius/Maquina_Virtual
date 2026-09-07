d179e618722485317f5fffa1c040555b1f8d59db
Tinisius <tinisalemmealonso@gmail.com>
2026-09-07 20:02:43 -0300

    declaraciones iniciales

--- main.c
+++ main.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include "utils.c"
+#include <string.h>
+#define N_MEM 16384
+#define N_SEG 8
+#define N_REG 32
+#define N_OP 32
+
+
+typedef struct {
+    char *name;
+    int value;
+}reg;
+
+typedef struct {
+    char *name;
+    short int code;
+    //puntero a func;
+}operatorASM;
 
 int main(int argc, char *argv[]) {
     printf("%d \n", argc);
-    
     printf("args: %s\n", *(argv+1));
-    //printf("El valor de 3 despues de 2 shifts es: %d\n", lsh(3, 2));
+
+    reg registers[N_REG];
+    int segments[N_SEG];
+    char memory[N_MEM];
+    operatorASM operations[N_OP];
+    
+    
 
     return 0;
 }
