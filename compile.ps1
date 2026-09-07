27c03b2c685db4c08c62a88a40ad926b0a92159f
Tinisius <tinisalemmealonso@gmail.com>
2026-09-07 19:43:33 -0300

    hola args

--- compile.ps1
+++ compile.ps1
@@ -8,6 +8,8 @@ if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
+& .\vmt\vmt.exe .\vmt\program.asm
+
 #y ejecuta el programa compilado
-& .\versions\MV.exe
+& .\versions\MV.exe hola.vmx
 exit $LASTEXITCODE
--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "utils.c"
 
-int main() {
-    printf("INICIO MAQUINOLA\n");
-    printf("El valor de 3 despues de 2 shifts es: %d\n", lsh(3, 2));
+int main(int argc, char *argv[]) {
+    printf("%d \n", argc);
+    
+    printf("args: %s\n", *(argv+1));
+    //printf("El valor de 3 despues de 2 shifts es: %d\n", lsh(3, 2));
+
     return 0;
 }
