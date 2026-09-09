691763a81f54fde91fe3ad69b824c7e2240aedf7
CamilaSanchezB <camilasanchezbonaldo@gmail.com>
2026-09-08 22:42:09 -0300

    feat: agrega funcion de lectura de cabecera y definicion del puntero a funcion para operaciones ASM

--- main.c
+++ main.c
@@ -1,33 +1,69 @@
 #include <stdio.h>
-#include "utils.c"
+#include <stdlib.h>
 #include <string.h>
+#include <stdint.h> //libreria para estandarizar el tamaño en bytes de los tipos
+#include "utils.c"
+
+#define N_HEADER 8
+#define ID "VMX26"
+#define VERSION 1
+
 #define N_MEM 16384
 #define N_SEG 8
 #define N_REG 32
 #define N_OP 32
 
-
-typedef struct {
+typedef struct
+{
     char *name;
     int value;
-}reg;
+} reg;
 
-typedef struct {
+typedef struct
+{
     char *name;
     short int code;
-    //puntero a func;
-}operatorASM;
+    void (*operation)(int, int);
+} operatorASM;
 
-int main(int argc, char *argv[]) {
+void readHeader(char route[], uint16_t *tamano_codigo, int *res)
+{
+    uint8_t line[N_HEADER];
+    FILE *arch = fopen(route, "rb");
+
+    if (fread(line, 1, N_HEADER, arch) == N_HEADER)
+    {
+        // Bytes 0-4: identificador "VMX26"
+        // Byte 5: version
+        // Bytes 6-7: tamano codigo
+        *tamano_codigo = ((uint16_t)line[6] << 8) | line[7];
+        //uso memcmp porque line no es una cadena terminada en \0. comparo byte a byte contra ID
+        *res = (memcmp(line, ID, 5) == 0) && (line[5] == VERSION) && ((*tamano_codigo) <= N_MEM - 1);
+
+         // TEST: mostrar lectura
+        printf("IDENTIFICADOR: \"%.5s\"\n", line);
+        printf("VERSION: %d\n", line[5]);
+        printf("TAMANO EN BYTES: %u\n", *tamano_codigo);
+        
+    }
+    else
+        *res = 0;
+    fclose(arch);
+}
+
+int main(int argc, char *argv[])
+{
     printf("%d \n", argc);
-    printf("args: %s\n", *(argv+1));
+    printf("args: %s\n", *(argv + 1));
+    
+    uint16_t tam_cod;
+    int res = 0;
+    readHeader(*(argv + 1), &tam_cod, &res);
 
-    reg registers[N_REG];
-    int segments[N_SEG];
-    char memory[N_MEM];
-    operatorASM operations[N_OP];
-    
-    
+    // reg registers[N_REG];
+    // int segments[N_SEG];
+    // char memory[N_MEM];
+    // operatorASM operations[N_OP];
 
     return 0;
 }
