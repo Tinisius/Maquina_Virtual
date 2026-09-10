7384a455968d6fc3e59893f8d844f3229ad71dff
Tinisius <tinisalemmealonso@gmail.com>
2026-09-10 10:58:20 -0300

    refactor: move constants to a separate header file

---
+++ constants.h
@@ -0,0 +1,9 @@
+#define N_HEADER 8
+#define ID "VMX26"
+#define VERSION 1
+
+#define N_MEM 16384
+#define N_SEG 8
+#define N_REG 32
+#define N_OP 32
+
--- main.c
+++ main.c
@@ -4,14 +4,7 @@
 #include <stdint.h> //libreria para estandarizar el tamaño en bytes de los tipos
 #include "utils.c"
 
-#define N_HEADER 8
-#define ID "VMX26"
-#define VERSION 1
-
-#define N_MEM 16384
-#define N_SEG 8
-#define N_REG 32
-#define N_OP 32
+#include "constants.h"
 
 typedef struct
 {
