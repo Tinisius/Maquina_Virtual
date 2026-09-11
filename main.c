e6672f3f98a3c53b30c4bae99fe5e4b1e62f6053
valenc2111 <valentincam1822@gmail.com>
2026-09-11 18:22:25 -0300

    creacion e inicializacion de la tabla de segmentos

--- main.c
+++ main.c
@@ -4,6 +4,29 @@
 #include "utils.c"
 #include "headers/operators.h"
 
+
+void createTableSeg (uint32_t TBS[], uint16_t cs_size){
+    TBS[0] = 0;
+    TBS[0] |= (cs_size & 0xFFFF);
+    TBS[1] = 0;
+    TBS[1] |= cs_size;
+    TBS[1] = TBS[1]<<16;
+    TBS[1] = TBS[1] | ( N_MEM - cs_size);
+
+     for(int i=2;i<N_SEG;i++){
+        TBS[i] = 0xFFFF;
+        TBS[i] = TBS[i] << 16;
+        TBS[i] = TBS[i] | 0xFFFF;
+    }
+   
+    /*for(int i=0;i<8;i++)
+        printf("0x%08X\n", TBS[i]);*/
+
+    
+    
+}
+
+
 void readHeader(char route[], uint16_t *code_size, int8_t *res)
 {
     uint8_t line[N_HEADER];
@@ -28,13 +51,14 @@ void readHeader(char route[], uint16_t *code_size, int8_t *res)
     fclose(arch);
 }
 
-void uploadMem(char *argv[], int8_t memory[]){
+void uploadMem(char *argv[], int8_t memory[],uint16_t *cs_size){
     FILE *arch = fopen(*(argv + 1), "rb");  //abre el archivo indicado por parametro
 
     uint16_t code_size; //guardamos el tamaño del code en una var de 2bytes
     int8_t res = 0;     //guarda si es posible ejecutar el programa .vmx
 
     readHeader(*(argv + 1), &code_size, &res);
+    *cs_size=code_size;
 
     if (res)
     {
@@ -48,6 +72,7 @@ void uploadMem(char *argv[], int8_t memory[]){
         {
             printf("%02x \t", memory[i]);   //muestra toda la memoria
         }
+        printf( "\n");
     }
 }
 
@@ -58,8 +83,10 @@ int main(int argc, char *argv[])
     operatorASM operators[N_OP] = OPERATORS;
 
     printf("%s\n",operators[0].name);   //muestra el operador de indice 0
+    uint16_t  cs_size;
+    uploadMem(argv, machine.memory,&cs_size);
+    createTableSeg(machine.segments,cs_size);  //se crea la tabla de segmentos
 
-    uploadMem(argv, machine.memory);
 
     return 0;
 }
